296e31f3409494ab088bacda3c4ab7c546e2cf23
unknown <arizalfikri7624@gmail.com>
2022-09-05 12:00:09 +0700

    feat: send page login github

--- server_side/app.js
+++ server_side/app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const axios = require("axios");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +11,10 @@ app.use(express.json());
 
 app.get("/github_signup", async (req, res) => {
   try {
+    let response = await axios.get(
+      `https://github.com/login/oauth/authorize?client_id=${process.env.client_id}`
+    );
+    res.send(response.data);
   } catch (error) {
     console.log(error);
   }
